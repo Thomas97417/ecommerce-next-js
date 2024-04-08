14698b7904c4a75ed1352dab45cff345616294bd
TOMA <throuquette@hotmail.fr>
2024-04-08 20:11:51 +0200

    add a custom purchase receipt email

---
+++ src/email/PurchaseReceipt.tsx
@@ -0,0 +1,60 @@
+import {
+  Body,
+  Container,
+  Head,
+  Html,
+  Preview,
+  Tailwind,
+  Text,
+} from "@react-email/components";
+import { OrderInformation } from "./components/OrderInformation";
+
+type PurchaseReceiptEmailProps = {
+  product: {
+    name: string;
+    imagePath: string;
+    description: string;
+  };
+  order: { id: string; createdAt: Date; pricePaidInCents: number };
+  downloadVerificationId: string;
+};
+
+PurchaseReceiptEmail.PreviewProps = {
+  product: {
+    name: "Product Name",
+    imagePath:
+      "/products/843718b4-c7e9-4a5b-975e-c1c2f264e439-istockphoto-517188688-612x612.jpg",
+    description: "Product description",
+  },
+  order: {
+    id: crypto.randomUUID(),
+    createdAt: new Date(),
+    pricePaidInCents: 10000,
+  },
+  downloadVerificationId: crypto.randomUUID(),
+} satisfies PurchaseReceiptEmailProps;
+
+export default function PurchaseReceiptEmail({
+  product,
+  order,
+  downloadVerificationId,
+}: PurchaseReceiptEmailProps) {
+  return (
+    <Html>
+      <Preview>Download {product.name} and view receipt</Preview>
+      <Tailwind>
+        <Head />
+        <Body className="font-sans bg-white">
+          <Container className="max-w-xl">
+            <Text className="text-3xl font-bold">Purchase Receipt</Text>
+            <OrderInformation
+              order={order}
+              product={product}
+              downloadVerificationId={downloadVerificationId}
+            />
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+}
